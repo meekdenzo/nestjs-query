3f37fa683dabac286b8e7a13e1a0368a9ef5be1a
Meek Denzo <meekdenzo@gmail.com>
2023-08-14 22:23:54 -0400

    test

--- examples/federation/graphql-gateway/src/app.module.ts
+++ examples/federation/graphql-gateway/src/app.module.ts
@@ -14,7 +14,7 @@ import { GraphQLModule } from '@nestjs/graphql'
       gateway: {
         serviceList: [
           { name: 'todo-items', url: 'http://localhost:3001/graphql' },
-          { name: 'sub-tasks', url: 'http://localhost:3002/graphql' },
+          // { name: 'sub-tasks', url: 'http://localhost:3002/graphql' },
           { name: 'tags', url: 'http://localhost:3003/graphql' },
           { name: 'user', url: 'http://localhost:3004/graphql' }
         ]
